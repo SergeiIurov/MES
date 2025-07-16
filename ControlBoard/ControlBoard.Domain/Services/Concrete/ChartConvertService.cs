17e199b03a79a15252422e1597c8883a2b298a4f
Sergei Iurov <drgrey2005@gmail.com>
2025-07-16 10:11:37 +0300

    Добавление механизма отображения типа продукта на доске контроля производства.

--- ControlBoard/ControlBoard.Domain/Services/Concrete/ChartConvertService.cs
+++ ControlBoard/ControlBoard.Domain/Services/Concrete/ChartConvertService.cs
@@ -4,21 +4,28 @@ using ControlBoard.Domain.Services.Abstract;
 
 namespace ControlBoard.Domain.Services.Concrete;
 
+/// <summary>
+/// Класс занимается преобразованием сырых XML данных доски с целью
+/// наполнения информацией всех элементов интерфейса, имеющих атрибут "sid".
+/// Значения данного атрибута сопоставляется с id станции. 
+/// </summary>
+/// <param name="repository"></param>
 public class ChartConvertService(IProcessStateRepository repository) : IChartConvertService
 {
     public async Task<string> Convert(string from)
     {
-        Dictionary<int, string> dict =
-            (await repository.GetLastProcessStateAsync()).ToDictionary(s => s.StationId, d => d.Value);
+        Dictionary<int, (string, string)> dict =
+            (await repository.GetLastProcessStateAsync()).ToDictionary(s => s.StationId,
+                d => (d.Value, d.ProductType?.Name ?? ""));
 
         XElement root = XElement.Parse(from);
         var data = root.Descendants("object").Where(e => e.Attribute("sid") != null);
 
         foreach (XElement elem in data)
         {
-            if (dict.TryGetValue(int.Parse(elem.Attribute("sid").Value), out string result))
+            if (dict.TryGetValue(int.Parse(elem.Attribute("sid").Value), out (string, string) result))
             {
-                elem.Attribute("label").Value = result;
+                elem.Attribute("label").Value = $"{result.Item1}\n{result.Item2}";
             }
         }
 

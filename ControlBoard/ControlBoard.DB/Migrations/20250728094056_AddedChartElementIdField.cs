53388dca7d10ad5e30e04a394d7c8ee62828e2fe
Sergei Iurov <drgrey2005@gmail.com>
2025-07-28 14:19:33 +0300

    Добавлена миграция.

---
+++ ControlBoard/ControlBoard.DB/Migrations/20250728094056_AddedChartElementIdField.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControlBoard.DB.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedChartElementIdField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "chart_element_id",
+                table: "stations",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_stations_chart_element_id",
+                table: "stations",
+                column: "chart_element_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_stations_chart_element_id",
+                table: "stations");
+
+            migrationBuilder.DropColumn(
+                name: "chart_element_id",
+                table: "stations");
+        }
+    }
+}

cff6a1323ea8b6f2f9de2f0622fcb1ceb5acfaee
Sergei Iurov <drgrey2005@gmail.com>
2025-07-31 16:10:29 +0300

    Очистка таблицы process_states перед обновлением текущего состояния доски контроля производства.

--- ControlBoard/ControlBoard.Domain/Services/Concrete/ProcessStateAdvService.cs
+++ ControlBoard/ControlBoard.Domain/Services/Concrete/ProcessStateAdvService.cs
@@ -1,19 +1,26 @@
-﻿using ControlBoard.DB.Entities;
+﻿using ControlBoard.DB;
+using ControlBoard.DB.Entities;
 using ControlBoard.DB.Repositories.Abstract;
 using ControlBoard.Domain.Dto;
 using ControlBoard.Domain.Services.Abstract;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace ControlBoard.Domain.Services.Concrete
 {
     public class ProcessStateAdvService(
         IProcessStateRepository repository,
+        MesDbContext context,
         ILogger<ProcessStateService> logger) : IProcessStateAdvService
     {
         public async Task SaveListAsync(List<ProcessStateAdvDto> list)
         {
             try
             {
+                logger.LogInformation("Подготовка к удалению текущего состояния доски контроля.");
+                //Удаляем состояние
+                context.Database.ExecuteSql($"truncate table process_states;");
+
                 logger.LogInformation("Подготовка информации к сохранению в БД");
                 Guid uid = Guid.NewGuid();
                 await repository.SaveProcessStatesAsync(list.Select(s =>

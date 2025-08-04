54c20e92ba0b3080ee1752d2b47fd74a5565ee63
Sergei Iurov <drgrey2005@gmail.com>
2025-08-04 13:05:15 +0300

    Доработан AuthController для реализации создания, удаления, вывода списка, и смены пароля пользователя.

--- ControlBoard/ControlBoard.Web/Controllers/AuthController.cs
+++ ControlBoard/ControlBoard.Web/Controllers/AuthController.cs
@@ -7,13 +7,15 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Net;
 using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 namespace ControlBoard.Web.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 [Authorize]
-public class AuthController(UserManager<ApplicationUser?> userManager, SignInManager<ApplicationUser?> signInManager) : ControllerBase
+public class AuthController(UserManager<ApplicationUser?> userManager, SignInManager<ApplicationUser?> signInManager)
+    : ControllerBase
 {
     [HttpPost]
     [AllowAnonymous]
@@ -24,7 +26,6 @@ public class AuthController(UserManager<ApplicationUser?> userManager, SignInMan
         ApplicationUser? user = await userManager.FindByNameAsync(loginInfo.UserName);
         if (user != null && (await signInManager.CheckPasswordSignInAsync(user, loginInfo.Password, true)).Succeeded)
         {
-
             var jwt = new JwtSecurityToken(
                 AuthOptions.Issuer,
                 AuthOptions.Audience,
@@ -48,7 +49,6 @@ public class AuthController(UserManager<ApplicationUser?> userManager, SignInMan
     [ProducesResponseType((int)HttpStatusCode.OK)]
     public async Task<ActionResult> CreateUser(UserInfo userInfo)
     {
-
         ApplicationUser? user = await userManager.FindByNameAsync(userInfo.Name);
         if (user == null)
         {
@@ -81,5 +81,50 @@ public class AuthController(UserManager<ApplicationUser?> userManager, SignInMan
         }
 
         return BadRequest();
+    }
+
+
+    //Получение списка пользователей с ролями
+    [HttpGet("Users")]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int)HttpStatusCode.Created)]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    public async Task<IEnumerable<UserInfo>> GetUsers()
+    {
+        List<UserInfo> users = new List<UserInfo>();
+        List<ApplicationUser?> appUsers = userManager.Users.ToList();
+        appUsers.ForEach(u =>
+        {
+            var claim = userManager.GetClaimsAsync(u).Result.FirstOrDefault(c => c.Type == ClaimTypes.Role);
+            users.Add(new UserInfo()
+            {
+                Name = u.UserName,
+                Role = MesRoles.TryParse(claim.Value, out MesRoles role) ? role : MesRoles.User
+            });
+        });
+        return users;
+    }
+
+    //Смена пароля
+    [HttpPost("ChangePassword")]
+    [ProducesResponseType((int)HttpStatusCode.Unauthorized)]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+    [ProducesResponseType((int)HttpStatusCode.Created)]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    public async Task<ActionResult> ChangePassworkd(UserInfo userInfo, string newPassword)
+    {
+        ApplicationUser? appUser = await userManager.FindByNameAsync(userInfo.Name);
+        if (appUser != null)
+        {
+            var result = await userManager.ChangePasswordAsync(appUser, userInfo.Password, newPassword);
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+        }
+
+        return BadRequest();
+
     }
 }

a892b4f95aca08659ae49e4d2442e657791cc10a
Sergei Iurov <drgrey2005@gmail.com>
2025-08-21 16:09:34 +0300

    Рефакторинг сервиса "ChartConvertService" - извлечение интерфейса.

---
+++ ControlBoard/ControlBoard.Domain/Services/Abstract/IChartServices.cs
@@ -0,0 +1,16 @@
+﻿using ControlBoard.DB.Entities.Enums;
+
+namespace ControlBoard.Domain.Services.Abstract
+{
+    public interface IChartServices
+    {
+        /// <summary>
+        /// Получение исполнения
+        /// </summary>
+        /// <param name="spec">На вход подаётся спецификация</param>
+        Task<string?> GetCarExecutionAsync(string? spec);
+        Task<string?> GetProductTypeAsync(string? spec, ProductTypes productType);
+        public string? GetCabinType(string? spec);
+
+    }
+}
---
+++ ControlBoard/ControlBoard.Domain/Services/Concrete/ChartServices.cs
@@ -0,0 +1,55 @@
+﻿using ControlBoard.DB.Entities.Enums;
+using ControlBoard.Domain.Services.Abstract;
+
+namespace ControlBoard.Domain.Services.Concrete
+{
+    public class ChartServices(ICarExecutionService carExecutionService) : IChartServices
+    {
+        /// <summary>
+        /// Получение исполнения
+        /// </summary>
+        /// <param name="spec">На вход подаётся спецификация</param>
+        public async Task<string?> GetCarExecutionAsync(string? spec)
+        {
+            Dictionary<string, string?> dict =
+                (await carExecutionService.GetCarExecutionsAsync()).ToDictionary(ce => ce.Code, ce => ce.Name);
+            if (spec is null)
+            {
+                return "";
+            }
+
+            return dict.TryGetValue(spec.Substring(4, 2), out string? value) ? value : "";
+        }
+
+        /// <summary>
+        /// Получение типа кабины
+        /// </summary>
+        /// <param name="spec">На вход подаётся спецификация </param>
+        public string? GetCabinType(string? spec)
+        {
+            if (spec is null)
+            {
+                return "";
+            }
+            return spec.Substring(2, 1) switch
+            {
+                "1" => "ККН",
+                "2" => "ККС",
+                "3" => "ДКН",
+                "4" => "ДКС",
+                "5" => "ДКВ",
+                _ => ""
+            };
+        }
+
+        public async Task<string?> GetProductTypeAsync(string? spec, ProductTypes productType)
+        {
+            return productType switch
+            {
+                ProductTypes.Cabina => GetCabinType(spec),
+                ProductTypes.Execution => await GetCarExecutionAsync(spec),
+                _ => ""
+            };
+        }
+    }
+}

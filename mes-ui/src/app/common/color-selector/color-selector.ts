adc9a5d09d3c41de977b34cc7831b8bbbf8b59db
Sergei Iurov <drgrey2005@gmail.com>
2025-08-26 14:49:43 +0300

    Доработка компонента "color-selector".

--- mes-ui/src/app/common/color-selector/color-selector.ts
+++ mes-ui/src/app/common/color-selector/color-selector.ts
@@ -11,10 +11,10 @@ import {NgStyle} from '@angular/common';
 })
 export class ColorSelector {
   @Input() selectedColor: string = 'red';
-
+  @Input() id: number = 0;
   @Output() onSelectedColorChange: EventEmitter<string> = new EventEmitter();
 
-  options = ['red', 'green', 'blue', 'purple', 'pink', 'yellow', 'black', 'orange','brown','darkblue', 'darkred','fuchsia','greenyellow'];
+  options = ['red', 'green', 'blue', 'purple', 'pink', 'yellow', 'black', 'orange', 'brown', 'darkblue', 'darkred', 'fuchsia', 'greenyellow'];
 
 
   changeColor(color: string) {
